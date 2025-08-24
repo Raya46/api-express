27ada169824fbf99cb23ae1ff783d1f3a7dc3989
Raya46 <muhammadrayaarrizki@gmail.com>
2025-08-24 23:06:21 +0700

    uprate limiter

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 500, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
